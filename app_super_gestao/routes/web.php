cd047b678595c62f3acb1ece0fd62008051e6299
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-11-21 20:56:02 -0300

    manipulating parameters

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -23,8 +23,8 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
-Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem?}',
-      function(string $nome, string $categoria, string $assunto, string $mensagem = 'Nenhuma mensagem'){
-        echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+Route::get('/contato/{nome}/{categoria_id}',
+      function(string $nome, int $categoria_id){
+        echo "Estamos aqui: $nome - $categoria_id";
       }
-);
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
