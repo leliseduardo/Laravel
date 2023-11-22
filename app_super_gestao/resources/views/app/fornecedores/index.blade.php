748ff5ce9b3c5d42aa2d21a2f1aa4696bdb6c18c
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-11-22 16:17:56 -0300

    for - Blade

--- app_super_gestao/resources/views/app/fornecedores/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedores/index.blade.php
@@ -18,27 +18,18 @@
 
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome'] }}
-    </br>
-    Status: {{ $fornecedores[0]['status'] }}
-    </br>
-    CNPJ: {{ $fornecedores[0]['cnpj'] ?? 'Não informado' }}
-    </br>
-    Telefone: {{ $fornecedores[0]['ddd'] ?? '' }} {{ $fornecedores[0]['telefone'] ?? '' }}
-    </br>
-    @switch($fornecedores[0]['ddd'])
-        @case('11')
-            São Paulo - SP
-            @break
-        @case('32')
-            Juiz de Fora - MG
-            @break;
-        @case('85')
-            Fortaleza - CE
-            @break
-        @default
-            Cidade não reconhecida
-    @endswitch
+    @for($i = 0; isset($fornecedores[$i]); $i++)
+        Fornecedor: {{ $fornecedores[$i]['nome'] }}
+        </br>
+        Status: {{ $fornecedores[$i]['status'] }}
+        </br>
+        CNPJ: {{ $fornecedores[$i]['cnpj'] ?? 'Não informado' }}
+        </br>
+        Telefone: {{ $fornecedores[$i]['ddd'] ?? '' }} {{ $fornecedores[$i]['telefone'] ?? '' }}
+        </br>
+
+        <hr>
+    @endfor
 @endisset
 
 
