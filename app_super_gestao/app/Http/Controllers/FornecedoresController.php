97fd656298208f91bc8438f2a94216bf7426e3af
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-11-22 13:42:21 -0300

    isset - Blade

--- app_super_gestao/app/Http/Controllers/FornecedoresController.php
+++ app_super_gestao/app/Http/Controllers/FornecedoresController.php
@@ -9,8 +9,13 @@ class FornecedoresController extends Controller
     public function index(){
         $fornecedores = [
             0 => [
-            'nome' => 'fornecedor 1',
-            'status' => 'N'
+                'nome' => 'fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '00.000.000/000.00'
+            ],
+            1 => [
+                'nome' => 'fornecedor 2',
+                'status' => 'S'
             ]
         ];
 
--- app_super_gestao/resources/views/app/fornecedores/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedores/index.blade.php
@@ -16,18 +16,23 @@
 
 {{-- @dd($fornecedores) // Imprime um array no navegador --}}
 
+
+@isset($fornecedores)
 Fornecedor: {{ $fornecedores[0]['nome'] }}
 </br>
 Status: {{ $fornecedores[0]['status'] }}
+</br>
+CNPJ: {{ $fornecedores[0]['cnpj'] }}
+</br>
+Fornecedor: {{ $fornecedores[1]['nome'] }}
+</br>
+Status: {{ $fornecedores[1]['status'] }}
+</br>
+@isset( $fornecedores[1]['cnpj'])
+CNPJ: {{ $fornecedores[1]['cnpj'] }}
+@endisset
 
-@if( !($fornecedores[0]['status'] == 'S'))
-    <h3> Fornecedor inativo </h3>
-@endif
-
-@unless($fornecedores[0]['status'] == "S")
-    <h3> Fornecedor inativo </h3>
-@endunless
-
+@endisset
 
 
 
