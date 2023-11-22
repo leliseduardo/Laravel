f13793dc25854e62e94ad1d80d187256aadf35ec
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-11-22 16:09:51 -0300

    switch/case - Blade

--- app_super_gestao/app/Http/Controllers/FornecedoresController.php
+++ app_super_gestao/app/Http/Controllers/FornecedoresController.php
@@ -11,11 +11,23 @@ class FornecedoresController extends Controller
             0 => [
                 'nome' => 'fornecedor 1',
                 'status' => 'N',
-                'cnpj' => '00.000.000/000.00'
+                'cnpj' => '00.000.000/000.00',
+                'ddd' => '11',
+                'telefone' => '00000-0000'
             ],
             1 => [
                 'nome' => 'fornecedor 2',
-                'status' => 'S'
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '32',
+                'telefone' => '00000-0000'
+            ],
+            2 => [
+                'nome' => 'fornecedor 3',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '85',
+                'telefone' => '00000-0000'
             ]
         ];
 
--- app_super_gestao/resources/views/app/fornecedores/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedores/index.blade.php
@@ -18,17 +18,27 @@
 
 
 @isset($fornecedores)
-Fornecedor: {{ $fornecedores[0]['nome'] }}
-</br>
-Status: {{ $fornecedores[0]['status'] }}
-</br>
-CNPJ: {{ $fornecedores[0]['cnpj'] ?? 'Não informado' }}
-</br>
-Fornecedor: {{ $fornecedores[1]['nome'] }}
-</br>
-Status: {{ $fornecedores[1]['status'] }}
-</br>
-CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Não informado' }}
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    </br>
+    Status: {{ $fornecedores[0]['status'] }}
+    </br>
+    CNPJ: {{ $fornecedores[0]['cnpj'] ?? 'Não informado' }}
+    </br>
+    Telefone: {{ $fornecedores[0]['ddd'] ?? '' }} {{ $fornecedores[0]['telefone'] ?? '' }}
+    </br>
+    @switch($fornecedores[0]['ddd'])
+        @case('11')
+            São Paulo - SP
+            @break
+        @case('32')
+            Juiz de Fora - MG
+            @break;
+        @case('85')
+            Fortaleza - CE
+            @break
+        @default
+            Cidade não reconhecida
+    @endswitch
 @endisset
 
 
