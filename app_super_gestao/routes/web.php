25bc04c24a07c6c2eabb6341786be58bed6d0a3f
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-11-21 21:31:21 -0300

    named routes

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -17,14 +17,14 @@ use Illuminate\Support\Facades\Route;
 //     return "Olá, seja bem vindo ao curso";
 // });
 
-Route::get('/', 'PrincipalController@principal');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-Route::get('/contato', 'ContatoController@contato');
-Route::get('/login', function(){ return 'Login';});
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){ return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function(){
-    Route::get('/clientes', function(){ return 'clientes';});
-    Route::get('/fornecedores', function(){ return 'fornecedores';});
-    Route::get('/produtos', function(){ return 'produtos';});
+    Route::get('/clientes', function(){ return 'clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'produtos';})->name('app.produtos');
 });
 
